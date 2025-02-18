7c590b3eab53f78adb7e9b4c3fd01c14ffa60734
Paucinha <paucia.renan@gmail.com>
2025-02-18 20:20:01 -0300

    alterado arquivo ConnectionConfig.java

--- src/main/java/br/com/dio/persistence/config/ConnectionConfig.java
+++ src/main/java/br/com/dio/persistence/config/ConnectionConfig.java
@@ -1,20 +1,19 @@
 package br.com.dio.persistence.config;
 
-import lombok.NoArgsConstructor;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import static lombok.AccessLevel.PRIVATE;
+import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = PRIVATE)
 public final class ConnectionConfig {
 
     public static Connection getConnection() throws SQLException {
         var url = "jdbc:mysql://localhost/board";
-        var user = "root";
-        var password = "V@mp1ra01";
+        var user = "";
+        var password = "";
         var connection = DriverManager.getConnection(url, user, password);
         connection.setAutoCommit(false);
         return connection;
